1e21bd842ba96c709b5b9b35cf4aaba6c20edec1
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2024-05-25 11:37:31 -0300

    Add favorites

--- src/app/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/pokemon-list-item/pokemon-list-item.component.ts
@@ -2,20 +2,41 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { NamedAPIResource, Pokemon } from '../services/interfaces';
 import { TitleCasePipe } from '@angular/common'
 import { addIcons } from 'ionicons';
-import { IonItem, IonLabel, IonNote, IonIcon, IonAvatar, IonImg } from '@ionic/angular/standalone'
+import {
+  IonItem,
+  IonLabel,
+  IonNote,
+  IonIcon,
+  IonAvatar,
+  IonImg,
+  IonButton
+} from '@ionic/angular/standalone'
 import { chevronForward } from 'ionicons/icons';
 import { PokemonService } from '../services/pokemon.service';
 import { RouterLink } from '@angular/router';
+import { FavoriteService } from '../services/favorite.service';
 
 @Component({
   selector: 'pokemon-list-item',
   templateUrl: './pokemon-list-item.component.html',
   styleUrls: ['./pokemon-list-item.component.scss'],
   standalone: true,
-  imports: [RouterLink, IonImg, IonAvatar, IonItem, IonLabel, IonNote, IonIcon, PokemonListItemComponent, TitleCasePipe]
+  imports: [
+    IonButton,
+    RouterLink,
+    IonImg,
+    IonAvatar,
+    IonItem,
+    IonLabel,
+    IonNote,
+    IonIcon,
+    PokemonListItemComponent,
+    TitleCasePipe
+  ]
 })
 export class PokemonListItemComponent implements OnInit {
   private pokemonService = inject(PokemonService);
+  private favoriteService = inject(FavoriteService);
 
   @Input() pokemonResource!: NamedAPIResource;
 
@@ -30,8 +51,19 @@ export class PokemonListItemComponent implements OnInit {
   }
 
   getPokemon() {
-    this.pokemonService.getPokemonByName(this.pokemonResource.name).subscribe((pokemonResutl) => {
-      this.pokemon = pokemonResutl;
-    })
+    this.pokemonService
+      .getPokemonByName(this.pokemonResource.name)
+      .subscribe((pokemonResutl) => {
+        this.pokemon = pokemonResutl;
+      })
+  }
+
+  isFavorite(pokemon: Pokemon): boolean {
+    return this.favoriteService.isFavorite(pokemon.name);
+  }
+
+  toggleFavorite(event: MouseEvent, pokemon: Pokemon) {
+    event.stopPropagation();
+    return this.favoriteService.toggleFavorite(pokemon.name);
   }
 }
---
+++ src/app/services/favorite.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, inject } from '@angular/core';
+import { Storage } from '@ionic/storage-angular'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+  private storage = inject(Storage);
+
+  private _storage?: Storage;
+  private key = 'favorites';
+  private favorites = new Set<string>();
+
+  constructor() {
+    this.init();
+  }
+
+  private async init() {
+    this._storage = await this.storage.create();
+    this.loadFavorites();
+  }
+
+  private async loadFavorites() {
+    const favorites = await this._storage?.get(this.key);
+    if (favorites) {
+      this.favorites = favorites;
+    }
+  }
+
+  isFavorite(name: string): boolean {
+    return this.favorites.has(name);
+  }
+
+  async toggleFavorite(name: string) {
+    if (this.isFavorite(name)) {
+      this.favorites.delete(name);
+    } else {
+      this.favorites.add(name);
+    }
+
+    await this._storage?.set(this.key, this.favorites);
+  }
+}
